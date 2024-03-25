a12bd5866125710c0cb57c4bed813b7616556322
yaniG <yane12girma@gmail.com>
2024-03-25 11:39:49 -0400

    Update 1-export_to_CSV.py

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -1,4 +1,3 @@
-
 #!/usr/bin/python3
 """ Export api to csv"""
 import csv
@@ -9,7 +8,7 @@ if __name__ == '__main__':
     user = sys.argv[1]
     url_user = 'https://jsonplaceholder.typicode.com/users/' + user
     res = requests.get(url_user)
-    """ANYTHING"""
+  
     user_name = res.json().get('username')
     task = url_user + '/todos'
     res = requests.get(task)
@@ -18,8 +17,8 @@ if __name__ == '__main__':
     with open('{}.csv'.format(user), 'w') as csvfile:
         for task in tasks:
             completed = task.get('completed')
-            """Complete"""
+          
             title_task = task.get('title')
-            """Done"""
+           
             csvfile.write('"{}","{}","{}","{}"\n'.format(
                 user, user_name, completed, title_task))
